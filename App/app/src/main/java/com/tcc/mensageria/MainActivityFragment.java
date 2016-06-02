e6f31b0fe851bb7d872fcdc5518827b563c49ef1
Born <pleasenerf@gmail.com>
2016-06-02 19:17:42 -0300

    Adicionando pastas

---
+++ App/app/src/main/java/com/tcc/mensageria/MainActivityFragment.java
@@ -0,0 +1,128 @@
+package com.tcc.mensageria;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private TextView textView;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            new TesteJson().execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        textView = (TextView) rootView.findViewById(R.id.texto_JSON);
+
+        //achei que tava lento por causa disso mas mesmo tirando continua lento D=
+        new TesteJson().execute();
+
+        return rootView;
+    }
+
+    public class TesteJson extends AsyncTask<Void, Void, String> {
+
+        private final String LOG_TAG = TesteJson.class.getSimpleName();
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String JsonString = null;
+
+            try {
+                String BASE_URL = "http://192.168.0.103:8080/cliente";
+
+                URL url = new URL(BASE_URL);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                JsonString = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return JsonString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                textView.setText(result);
+                Log.d(LOG_TAG, (String) textView.getText());
+            }
+        }
+
+    }
+
+
+}
