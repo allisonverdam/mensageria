0a622b1ab8f23f8ed64929c0c5e5b3c6d53451d0
muggyboy <renanryo@gmail.com>
2016-08-31 22:55:11 -0300

    configuração de endereço
    adicionada configuração temporaria do endereço do web service

--- App/app/src/main/java/com/tcc/mensageria/View/MainActivity.java
+++ App/app/src/main/java/com/tcc/mensageria/View/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tcc.mensageria.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this,PreferenciasActivity.class));
             return true;
         }
 
